c6a5598b98a6fc2ad2fcbc8d1b24676db61b9d42
Yoni Slotwinere <yonislots@gmail.com>
2018-09-11 16:19:57 -0400

    added fetch post request; successfully routing in server

--- public/upload.js
+++ public/upload.js
@@ -1,1 +1,35 @@
 console.log('HEY AGAIN')
+
+document.getElementById('upload-form').addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+
+    var formData = new FormData();
+    formData.append("upload[file]", document.getElementById("file").files[0]);
+
+    fetch('/api/upload', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({formData}),
+    })
+    .then(response => {
+    })
+
+}
+
+
+function onUploadProgress(event) {
+    if (event.lengthComputable) {
+        var percentComplete = event.loaded / event.total;
+        document.getElementById('progress-value').textContent = parseFloat(percentComplete*100).toFixed(2);
+    }
+}
+
+function onRequestComplete() {
+  console.log('request complete!')
+}
+
+function onUploadComplete() {
+  console.log('upload complete!')
+}
--- server/api/upload.js
+++ server/api/upload.js
@@ -1,3 +1,7 @@
 const router = require('express').Router()
 
+router.post('/', (req, res, next) => {
+  console.log('REQ!', req.body)
+})
+
 module.exports = router
