5242aea0f63f7eb09b583ebae762e6e889afddfc
Yoni Slotwinere <yonislots@gmail.com>
2018-09-12 22:02:43 -0400

    added notes and readme documentation

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# mime-type-detection
+# MIME Type Detection and Verification
 
-- Added detection for 'video/mpeg'
+This app correctly detecs and verifies the following MIME types:
+- video/mp4
+- video/quicktime
+- video/x-msvideo
+- video/x-ms-wmv
+- video/webm
+- video/ogg
+- video/mpeg
+
+Note that the last one on the list was added for support beyond the basic requirements.
+
+After some thought, I decided to build this client-side only. Of course, there are trade-offs, but for this purpose I thought a wiser choice is performing the validation on the front-end while avoiding unnecessary waste of server resource. If back-end validation is required, I am happy to add that here, as well.
+
+I used a JavaScript object to store the first few characters converted from the hex signature of each file as the keys, and the corresponding file type as their values. I chose an object for constant time lookup as opposed to an array, which would have, at worst, linear, or O(n) time complexity.
+
+Please let me know if you have any questions!
+
+- Yoni Slotwiner
--- src/upload.js
+++ src/upload.js
@@ -10,20 +10,25 @@ const validMimeTypes = {
   '001ba': 'video/mpeg',
 };
 
+// Add event listener with an 'on submit' callback, for the "verify" button
 document.getElementById('upload-form').addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault()
   const blob = document.getElementById("file").files[0];
+  // Create a new instance of FileReader, which allows to asynchronously read the contents of files. More info here https://developer.mozilla.org/en-US/docs/Web/API/FileReader
   const fileReader = new FileReader();
 
+  // When file finishes loading, create a new instance of Uint8Array and assign it the first few bytes from the payload
   fileReader.onloadend = function(e) {
     const arr = (new Uint8Array(e.target.result)).subarray(0, 4);
+    // Convert each element of arr to it's string format
     let header = "";
     for(let i = 0; i < arr.length; i++) {
        header += arr[i].toString(16);
     }
     header = header.slice(0, 5)
+    // Conditionally render HTML based on whether header is found in the validMimeTypes object
     if(header in validMimeTypes) {
       mimeType.innerHTML = `MIME Type: ${validMimeTypes[header]}`;
       message.innerHTML = 'Valid file type!';
@@ -35,7 +40,7 @@ function onSubmit(event) {
       message.style.color = 'red';
     }
   };
-
+  // Read the contents of blob to get the file's data
   fileReader.readAsArrayBuffer(blob);
 }
 
